dfd3891540dac3dfb2233b72acb5caa3c397fef6
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-11-21 16:23:16 +0100

    separated card script

--- docs/script.js
+++ docs/script.js
@@ -12,16 +12,6 @@ window.onmousemove = e => {
     
 }
 
-document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
-        const rect = card.getBoundingClientRect();
-        x = e.clientX - rect.left;
-        y = e.clientY - rect.top;
-        card.style.setProperty("--mouse-x", `${x}px`);
-        card.style.setProperty("--mouse-y", `${y}px`);
-    };
-}
-
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*()_+`-={}|[]\\:\";'<>?,./";
 let interval = [];
 const flaps = document.getElementsByClassName("splitflap");
