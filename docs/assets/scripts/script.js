9dc124f8a63cb820675ad5aa54acba15e0fdfffe
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2024-07-13 13:26:56 +0200

    head without ejs title gets set by js

--- docs/assets/scripts/script.js
+++ docs/assets/scripts/script.js
@@ -1,3 +1,7 @@
+var title = document.getElementsByName('title')[0].content;
+console.log(title);
+document.title = "x42.space | " + title;
+
 function goTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
